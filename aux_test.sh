b5077e2fbf90de36d92bdf314ac5ad9e21b46c98
pj12520 <paul.jarvis@bristol.ac.uk>
2015-10-01 10:31:00 +0100

    Modified submissions scripts for submitting sweep over auilery varaibles

--- aux_test.sh
+++ aux_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd testing/aux_param/data3
+cd testing/aux_param/data4
 
 for i in $(ls -d */); do
 
@@ -14,9 +14,9 @@ for i in $(ls -d */); do
 
 	    cd ${k}
 
-	    export INPUT=${HOME}/sinking_bim/trunk/testing/aux_param/data3/${i}/${j}/${k}/dimensionless_input.dat
+	    export INPUT=${HOME}/sinking_bim/trunk/testing/aux_param/data4/${i}/${j}/${k}/dimensionless_input.dat
 	    
-	    export RUNDIR=${HOME}/sinking_bim/trunk/testing/aux_param/data3/${i}/${j}/${k}
+	    export RUNDIR=${HOME}/sinking_bim/trunk/testing/aux_param/data4/${i}/${j}/${k}
 
 	    qsub -V ../../../../../../param_sweep.sh
 
