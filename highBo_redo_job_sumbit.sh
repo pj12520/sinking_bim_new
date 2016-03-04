130a6f2af086c05c6e2581c553f79b01e5ed4c8b
pj12520 <paul.jarvis@bristol.ac.uk>
2016-03-04 14:32:36 +0000

    Input files to rerun simulations for high Bo all with an initial height of 11

---
+++ highBo_redo_job_sumbit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd highBo_redo_data
+
+for i in $(ls -d */); do
+
+    cd ${i}
+
+    for j in $(ls -d */); do
+
+	cd ${j}
+
+	for k in $(ls -d */); do
+
+	    cd ${k}
+
+	    export INPUT=${HOME}/sinking_bim/trunk/highBo_redo_data/${i}/${j}/${k}/dimensionless_input.dat
+
+	    export RUNDIR=${HOME}/sinking_bim/trunk/highBo_redo_data/${i}/${j}/${k}
+
+	    qsub -V ../../../../param_sweep.sh
+
+	    sleep 10
+
+	    cd ..
+
+	done
+
+	cd ..
+
+    done
+
+    cd ..
+
+done
+
+cd ..
