19e2990dcea592c87e0ca7313ec67f883a2648af
pj12520 <paul.jarvis@bristol.ac.uk>
2016-03-06 11:25:02 +0000

    Fixed input file for low Bo snap simulations

--- input_create_lowBo_snap.cc
+++ input_create_lowBo_snap.cc
@@ -82,91 +82,6 @@ int main()
 
   chdir(data);
 
-  for (int i = 0; i < 6; i++)
-    {
-      D_convert << "D=" << mod_dens_rat_data[i];
-
-      D_dir_name = D_convert.str();
-
-      mkdir(D_dir_name.c_str(), S_IRWXU);
-
-      for (int j = 9; j < bond_data.size(); j++)
-	{
-	  Bo_convert << "Bo=" << bond_data[j];
-
-	  Bo_dir_name = Bo_convert.str();
-
-	  chdir(D_dir_name.c_str());
-
-	  mkdir(Bo_dir_name.c_str(), S_IRWXU);
-
-	  for (int k = 0; k < viscos_rat_data.size(); k++)
-	    {
-	      viscos_convert << "viscos_rat=" << viscos_rat_data[k];
-
-	      viscos_dir_name = viscos_convert.str();
-
-	      chdir(Bo_dir_name.c_str());
-
-	      mkdir(viscos_dir_name.c_str(), S_IRWXU);
-
-	      chdir(viscos_dir_name.c_str());
-
-	      ofstream fout;
-
-	      fout.open("dimensionless_input.dat");
-
-	      fout << bond_data[j] << endl;
-	      fout << mod_dens_rat_data[i] << endl;
-	      fout << viscos_rat_data[k] << endl;
-
-	      fout << n_sphere << endl;
-	      fout << n_interf << endl;
-	      fout << trunc << endl;
-	      //	      fout << t_step << endl;
-	      fout << height << endl;
-	      fout <<max_it << endl;
-	      fout << aspect << endl;
-	      fout << diff_step << endl;
-	      fout << n_out << endl;
-
-	      fout << "Input file containing the dimensionless numbers that characterise the system" << endl;
-
-	      fout << "Bond Number" << endl;
-	      fout << "Modified Density Ratio" << endl;
-	      fout << "Viscosity Ratio" << endl;
-
-	      fout << "Number of elements on sphere" << endl;
-	      fout << "Number of elements on interface" << endl;
-	      fout << "Truncation length of interface" << endl;
-	      //	      fout << "Time step" << endl;
-	      fout << "Initial height of sphere" << endl;
-	      fout << "Maximum number of iterations" << endl;
-	      fout << "Aspect Ratio" << endl;
-	      fout << "Initial step size used in the numerical differentiation" << endl;
-	      fout << "Number of times at which output occurs" << endl;
-	      fout.close();
-
-	      chdir(up);
-	      
-	      chdir(up);
-
-	      viscos_convert.str(string());
-
-	    }
-
-	  chdir(up);
-
-	  Bo_convert.str(string());
-
-	}
-      D_convert.str(string());
-
-    }
-
-
-
-
   //Now D = 50
 
   for (int i = 6; i < 7; i++)
@@ -189,93 +104,6 @@ int main()
 
 	  for (int k = 0; k < viscos_rat_data.size(); k++)
 	    {
-	      viscos_convert << "viscos_rat=" << viscos_rat_data[2 * k + 1];
-
-	      viscos_dir_name = viscos_convert.str();
-
-	      chdir(Bo_dir_name.c_str());
-
-	      mkdir(viscos_dir_name.c_str(), S_IRWXU);
-
-	      chdir(viscos_dir_name.c_str());
-
-	      ofstream fout;
-
-	      fout.open("dimensionless_input.dat");
-
-	      fout << bond_data[j] << endl;
-	      fout << mod_dens_rat_data[i] << endl;
-	      fout << viscos_rat_data[2 * k + 1] << endl;
-
-	      fout << n_sphere << endl;
-	      fout << n_interf << endl;
-	      fout << trunc << endl;
-	      //	      fout << t_step << endl;
-	      fout << height << endl;
-	      fout <<max_it << endl;
-	      fout << aspect << endl;
-	      fout << diff_step << endl;
-	      fout << n_out << endl;
-
-	      fout << "Input file containing the dimensionless numbers that characterise the system" << endl;
-
-	      fout << "Bond Number" << endl;
-	      fout << "Modified Density Ratio" << endl;
-	      fout << "Viscosity Ratio" << endl;
-
-	      fout << "Number of elements on sphere" << endl;
-	      fout << "Number of elements on interface" << endl;
-	      fout << "Truncation length of interface" << endl;
-	      //	      fout << "Time step" << endl;
-	      fout << "Initial height of sphere" << endl;
-	      fout << "Maximum number of iterations" << endl;
-	      fout << "Aspect Ratio" << endl;
-	      fout << "Initial step size used in the numerical differentiation" << endl;
-	      fout << "Number of times at which output occurs" << endl;
-	      fout.close();
-
-	      chdir(up);
-	      
-	      chdir(up);
-
-	      viscos_convert.str(string());
-
-	    }
-
-	  chdir(up);
-
-	  Bo_convert.str(string());
-
-	}
-      D_convert.str(string());
-
-    }
-
-
-
-
-  //D = 100 - 1000
-
-  for (int i = 7; i < mod_dens_rat_data.size(); i++)
-    {
-      D_convert << "D=" << mod_dens_rat_data[i];
-
-      D_dir_name = D_convert.str();
-
-      mkdir(D_dir_name.c_str(), S_IRWXU);
-
-      for (int j = 0; j < bond_data.size(); j++)
-	{
-	  Bo_convert << "Bo=" << bond_data[j];
-
-	  Bo_dir_name = Bo_convert.str();
-
-	  chdir(D_dir_name.c_str());
-
-	  mkdir(Bo_dir_name.c_str(), S_IRWXU);
-
-	  for (int k = 0; k < viscos_rat_data.size(); k++)
-	    {
 	      viscos_convert << "viscos_rat=" << viscos_rat_data[k];
 
 	      viscos_dir_name = viscos_convert.str();
@@ -337,6 +165,10 @@ int main()
       D_convert.str(string());
 
     }
+
+
+
+
 
   return 0;
 }
